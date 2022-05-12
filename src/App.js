09a6c7ddcecbec0c7a617a4843818d3a3acc9b9f
unknown <howell2028@yahoo.com>
2022-05-12 12:55:39 +0100

    create todo-list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import TodoList from './components/TodoList';
+import FormTodo from './components/FormTodo';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(
+    [
+      {
+        id:  Math.floor(Math.random() * 1000),
+        task: 'Simba Coding Challenge',
+        isCompleted: true
+      }
+    ]
+  );
+
+  const addTodo = task => {
+    const newTodos  = [
+      ...todos, { task, isCompleted: false, id: Math.floor(Math.random() * 1000) } 
+    ]
+    setTodos(newTodos)
+  }
+
+  const removeTodo = id => {
+    const newTodos  = todos.filter(todo => {
+      return todo.id !== id
+    })
+    setTodos(newTodos)
+  }
+
+  const handleToggle = id => {
+    let mapped = todos.map(todo => {
+      return todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : { ...todo }
+    })
+    setTodos(mapped);
+  }
+
+  const editTask = (id, newTask) => {
+    const editedTask = todos.map(todo => {
+      return todo.id === id ? { ...todo, task: newTask } : todo 
+    })
+    setTodos(editedTask);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header text="TODO LIST APP" />
+      <section className='container'>
+        <FormTodo  
+          addTodo={addTodo}
+        />
+        <TodoList 
+          todos={todos}
+          removeTodo={removeTodo}
+          handleToggle={handleToggle}
+          editTask={editTask}
+        />
+      </section>
+    </>
   );
 }
 
